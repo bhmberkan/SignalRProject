d376408d9cef5a952ed5e1694e31da348bf02319
bhmberkan <berkanburakturgut@gmail.com>
2025-10-21 23:42:12 +0300

    Add project files.

---
+++ SignalR.EntityLayer/Entities/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace SignalR.EntityLayer.Entities
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public String Name { get; set; }
+        public bool Status { get; set; }
+    }
+}

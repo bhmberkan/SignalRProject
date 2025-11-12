0e86b203e3c651e41e011fb735e653cdee86ba90
bhmberkan <berkanburakturgut@gmail.com>
2025-11-12 23:25:50 +0300

    SignalR kısmını devam ettirdim, Silder kısmı için api kodladım, mig geçtim, UI kısmına başladım, Layout oluşturdum, güncellemelerini yaptım, Default oluşturup componentlere ayırdım, ilgili kısımların consume işlemlerini gerçekleştiriyorum.

---
+++ SignalR.DtoLayer/SliderDto/GetByIdSliderDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.DtoLayer.SliderDto
+{
+    public class GetByIdSliderDto
+    {
+        public int SliderID { get; set; }
+        public String Title1 { get; set; }
+        public String Title2 { get; set; }
+        public String Title3 { get; set; }
+        public String Description1 { get; set; }
+        public String Description2 { get; set; }
+        public String Description3 { get; set; }
+    }
+}
